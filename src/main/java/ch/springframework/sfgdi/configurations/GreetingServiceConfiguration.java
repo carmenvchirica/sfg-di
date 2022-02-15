f0cefc565ac12f2dd94dc5b301da79d1478accae
Carmen Chirica <carmen@globalsoftsys.ro>
2022-02-15 16:44:42 +0200

    Implementation of @repository into Configuration class

--- src/main/java/ch/springframework/sfgdi/configurations/GreetingServiceConfiguration.java
+++ src/main/java/ch/springframework/sfgdi/configurations/GreetingServiceConfiguration.java
@@ -1,15 +1,40 @@
 package ch.springframework.sfgdi.configurations;
 
-import ch.springframework.sfgdi.services.ConstructorGreetingService;
-import ch.springframework.sfgdi.services.PropertyGreetingService;
-import ch.springframework.sfgdi.services.SetterGreetingService;
+import ch.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import ch.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
+import ch.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfiguration {
 
     // A @Configuration class will define another @Bean
+
+    @Bean
+    @Profile("EN")
+    I18nEnglishService i18nService(EnglishGreetingRepository englishGreetingRepository) {
+        return new I18nEnglishService(englishGreetingRepository);
+    }
+
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository() {
+        return new EnglishGreetingRepositoryImpl();
+    }
+
+    @Bean("i18nService")
+    @Profile({"ES", "default"})
+    I18nSpanishService i18nSpanishService() {
+        return new I18nSpanishService();
+    }
+
+    @Bean
+    @Primary
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService() {
