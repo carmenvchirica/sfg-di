c13641dde5c4438e790458379f3117a624b7fb53
Carmen Chirica <carmen@globalsoftsys.ro>
2022-02-15 16:44:17 +0200

    Implementation of @repository

--- src/main/java/ch/springframework/sfgdi/services/I18nEnglishService.java
+++ src/main/java/ch/springframework/sfgdi/services/I18nEnglishService.java
@@ -1,14 +1,22 @@
 package ch.springframework.sfgdi.services;
 
+import ch.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
-@Service("i18nService")
+//@Profile("EN")
+//@Service("i18nService")
 public class I18nEnglishService implements GreetingService {
+
+    private final EnglishGreetingRepository englishGreetingRepository;
+
+    public I18nEnglishService(EnglishGreetingRepository englishGreetingRepository) {
+        this.englishGreetingRepository = englishGreetingRepository;
+    }
 
     @Override
     public String sayGreeting() {
-        return "Hello World - EN";
+        return englishGreetingRepository.getGreeting();
     }
+
 }
